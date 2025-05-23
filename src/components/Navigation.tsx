6d1b1b5234d60c6ba974781bdad24e14e7b0c980
SukhvirKooner <sukhvirkooner05@gmail.com>
2025-05-23 22:03:11 +0530

    updated navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 
 const Navigation = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-black/80 backdrop-blur-sm border-b border-gray-800">
+    <nav className="fixed top-0 left-0 right-0 z-50  border-gray-800">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         <div className="flex items-center space-x-8">
           <div className="text-white font-bold text-xl italic">
